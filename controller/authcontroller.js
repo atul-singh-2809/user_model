d256b8947a233f566e4f47c7a0157b15e1d11d0a
Atul <singhatul897@gmail.com>
2020-07-21 23:48:15 +0530

    login added

--- controller/authcontroller.js
+++ controller/authcontroller.js
@@ -1,34 +1,70 @@
 const User = require('../models/user_model')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
-const register =(req,res,next) => { 
-    bcrypt.hash(req.body.password,10,function(err,hashedpass){
-        if(err){
+const register = (req, res, next) => {
+    bcrypt.hash(req.body.password, 10, function (err, hashedpass) {
+        if (err) {
             res.json({
-                error:err
+                error: err
             })
         }
         let user = new User({
-            name : req.body.name,
-            email : req.body.email,
-            phone : req.body.phone,
-            password : hashedpass
+            name: req.body.name,
+            email: req.body.email,
+            phone: req.body.phone,
+            password: hashedpass
         })
         user.save()
-        .then(user=>{
-            res.json({
-                message:'user added successfully'
+            .then(user => {
+                res.json({
+                    message: 'user added successfully'
+                })
             })
-        })
-        .catch(error=>{
-            res.json({
-                message:'an error occured'
+            .catch(error => {
+                res.json({
+                    message: 'an error occured'
+                })
             })
-        })
     })
-  
+
 }
 
-module.exports={
-    register
+// login
+const login = (req, res, next) => {
+    var username = req.body.username
+    var password = req.body.password
+
+
+    User.findOne({ $or: [{ email: username }, { phone: username }] })
+        .then(user => {
+            if (user) {
+                bcrypt.compare(password, user.password, function (err, result) {
+                    if (err) {
+                        res.json({
+                            error: err
+                        })
+                    }
+                    if (result) {
+                        let token = jwt.sign({ name: user.name }, 'verySecretValue', { expiresIn: '1hr' })
+                        res.json({
+                            message: 'login successful',
+                            token
+                        })
+                    }
+                    else {
+                        res.json({
+                            message: 'password does not matches'
+                        })
+                    }
+                })
+            } else {
+                res.json({
+                    message: 'no user found'
+                })
+            }
+        })
+}
+
+module.exports = {
+    register,login
 }
--- routes/auth.js
+++ routes/auth.js
@@ -4,5 +4,6 @@ const router = express.Router()
 const authcontroller = require('../controller/authcontroller')
 
 router.post('/register',authcontroller.register)
+router.post('/login',authcontroller.login)
 
 module.exports = router
